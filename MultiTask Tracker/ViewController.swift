0ecd77a96a1ed1400e18a5fab8a09a00487cc394
YonYoon <zhzhzh1806@gmail.com>
2024-07-11 17:41:12 +0500

    Add welcome message label to registration view

--- MultiTask Tracker/ViewController.swift
+++ MultiTask Tracker/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let welcomeMessageLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         
+        configureWelcomeMessageLabel()
     }
-
-
+    
+    func configureWelcomeMessageLabel() {
+        view.addSubview(welcomeMessageLabel)
+        welcomeMessageLabel.translatesAutoresizingMaskIntoConstraints = false
+        welcomeMessageLabel.text = "🎉Welcome to Multitask Tracker"
+        welcomeMessageLabel.textAlignment = .center
+        welcomeMessageLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        welcomeMessageLabel.adjustsFontForContentSizeCategory = true
+        welcomeMessageLabel.numberOfLines = 2
+        
+        
+        NSLayoutConstraint.activate([
+            welcomeMessageLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 39),
+            welcomeMessageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            welcomeMessageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            welcomeMessageLabel.heightAnchor.constraint(equalToConstant: 33)
+        ])
+    }
 }
 
+#Preview {
+    ViewController().view
+}
