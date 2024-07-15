4290ae4d2a874594b2d9f7e71571bdea3ab4994b
YonYoon <zhzhzh1806@gmail.com>
2024-07-15 20:35:19 +0500

    Change bell image to be 2x

---
+++ MultiTask Tracker/StoriesView.swift
@@ -0,0 +1,20 @@
+//
+//  StoriesView.swift
+//  MultiTask Tracker
+//
+//  Created by Zhansen Zhalel on 15.07.2024.
+//
+
+import UIKit
+
+class StoriesView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
