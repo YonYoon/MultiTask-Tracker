c438028a3270d160e6ebc9b2ccd444bd56572c30
YonYoon <zhzhzh1806@gmail.com>
2024-07-26 23:07:27 +0500

    Add firebase to project

--- MultiTask Tracker/AppDelegate.swift
+++ MultiTask Tracker/AppDelegate.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseAuth
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
         return true
     }
 
