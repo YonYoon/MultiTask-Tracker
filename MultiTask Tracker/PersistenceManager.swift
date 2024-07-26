07ec2fddeaad16c09fa79a23c9179fc09da8eae6
YonYoon <zhzhzh1806@gmail.com>
2024-07-26 23:10:18 +0500

    Make read and write of friends in memory

--- MultiTask Tracker/PersistenceManager.swift
+++ MultiTask Tracker/PersistenceManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class PersistenceManager {
+    static let shared = PersistenceManager()
+    static private let url = URL.documentsDirectory.appending(path: "friends.data")
+    
+    func read() -> [MTUser] {
+        do {
+            let data = try Data(contentsOf: PersistenceManager.url)
+            let friends = try JSONDecoder().decode([MTUser].self, from: data)
+            return friends
+        } catch {
+            print(error)
+            return []
+        }
+    }
+    
+    func write(friends: [MTUser]) {
+        do {
+            let data = try JSONEncoder().encode(friends)
+            try data.write(to: PersistenceManager.url)
+            print("succesfully written")
+        } catch {
+            print(error)
+        }
+    }
+}
