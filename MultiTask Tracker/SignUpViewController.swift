8371fd44625540d9a81572949bec81f7d6387219
YonYoon <zhzhzh1806@gmail.com>
2024-07-27 16:30:46 +0500

    Add text fields for sign up view

--- MultiTask Tracker/SignUpViewController.swift
+++ MultiTask Tracker/SignUpViewController.swift
@@ -8,22 +8,64 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    var nameField = UITextField()
+    var emailField = UITextField()
+    var passwordField = UITextField()
+    var confirmPasswordField = UITextField()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        navigationItem.title = "Sign-up"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        configureNameField()
+        configureEmailField()
+        configurePasswordField()
+        configureConfirmPasswordField()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureNameField() {
+        setup(textField: nameField, placeholderText: "Name")
+        
+        nameField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15).isActive = true
     }
-    */
+    
+    private func configureEmailField() {
+        setup(textField: emailField, placeholderText: "Email")
+        
+        emailField.topAnchor.constraint(equalTo: nameField.bottomAnchor, constant: 15).isActive = true
+    }
+    
+    private func configurePasswordField() {
+        setup(textField: passwordField, placeholderText: "Password")
+        
+        passwordField.topAnchor.constraint(equalTo: emailField.bottomAnchor, constant: 15).isActive = true
+    }
+    
+    private func configureConfirmPasswordField() {
+        setup(textField: confirmPasswordField, placeholderText: "Confirm password")
+        
+        confirmPasswordField.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 15).isActive = true
+    }
+    
+    private func setup(textField: UITextField, placeholderText: String) {
+        view.addSubview(textField)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        
+        textField.borderStyle = .roundedRect
+        textField.placeholder = placeholderText
+        
+        NSLayoutConstraint.activate([
+            textField.heightAnchor.constraint(equalToConstant: 50),
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            textField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
+        ])
+    }
+}
 
+#Preview {
+    let navigationController = UINavigationController(rootViewController: SignUpViewController())
+    return navigationController.view
 }
