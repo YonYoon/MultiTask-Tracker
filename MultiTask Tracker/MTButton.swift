a98cf05f3c30a8edaf1d7d32b8fa02c0901d042b
YonYoon <zhzhzh1806@gmail.com>
2024-07-28 20:55:55 +0500

    Create MTButton custom class file

---
+++ MultiTask Tracker/MTButton.swift
@@ -0,0 +1,20 @@
+//
+//  MTButton.swift
+//  MultiTask Tracker
+//
+//  Created by Zhansen Zhalel on 28.07.2024.
+//
+
+import UIKit
+
+class MTButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
