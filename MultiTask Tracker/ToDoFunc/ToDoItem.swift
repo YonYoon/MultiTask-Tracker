65b621aa738989137f5df93d99fe7210295025b7
Volkov <alexey81029@gmail.com>
2024-07-29 16:43:06 +0300

    Created comments

--- MultiTask Tracker/ToDoFunc/ToDoItem.swift
+++ MultiTask Tracker/ToDoFunc/ToDoItem.swift
@@ -7,9 +7,16 @@
 
 import Foundation
 
-struct ToDoItem {
+// Codable - это протокол, который делает структуру доступной для кодирования (преобразования в JSON) и декодирования (преобразования из JSON) данных.
+
+struct ToDoItem: Codable {
     var name: String
     var isCompleted: Bool = false
     var id: UUID = UUID()
 }
 
+/* var id: UUID = UUID()
+ Объявляет свойство id типа UUID (уникальный идентификатор)
+UUID - это тип данных, который генерирует уникальные идентификаторы, используемые для идентификации задач.
+По умолчанию, при создании  ToDoItem  генерируется новый  UUID.
+*/
--- MultiTask Tracker/ToDoViewController.swift
+++ MultiTask Tracker/ToDoViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+// Task list storage
     var todos: [ToDoItem] = [ToDoItem(name: "Test todo item")]
     
     override func viewDidLoad() {
@@ -19,7 +20,8 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
         constraintTableView()
         configureTableView()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openAlert))
+// Сreates an "Add" button in the top right corner of the screen that, when clicked, calls the `openAlert` function
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(openAlert))
     }
     
     var tableView: UITableView = {
@@ -28,27 +30,7 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
         return tableView
     }()
     
-        @objc func openAlert() {
-            let alert = UIAlertController(title: "Create todo", message: "", preferredStyle: .alert)
-            alert.addTextField()
-            let saveButton = UIAlertAction(title: "Save", style: .default) { _ in
-                if let textName = alert.textFields?.first?.text {
-                    self.addTodo(name: textName)
-            }
-        }
-        alert.addAction(saveButton)
-            let cancelButton = UIAlertAction(title: "Cancel", style: .destructive)
-            alert.addAction(cancelButton )
-            
-            present(alert, animated: true)
-        }
-    
-    func addTodo(name: String){
-        todos.append(ToDoItem(name: name))
-        tableView.reloadData()
-    }
-    
-    
+// Full page constraint
     func constraintTableView() {
         NSLayoutConstraint.activate([
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -58,18 +40,46 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
             ])
     }
     
-    func configureTableView(){
+// Sets the current controller as the data source and table delegate, registers the cell type, and disables cell selection
+    func configureTableView() {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "todoCell")
         tableView.allowsSelection = false
     }
     
-    //MARK: - Table View Methods
+// MARK: - Alert
+    
+// Function for Alert (adds a new task to the list ToDo and updates the page)
+    func addTodo(name: String) {
+        todos.append(ToDoItem(name: name))
+        tableView.reloadData()
+        }
+
+// Create Alert
+    @objc func openAlert() {
+        let alert = UIAlertController(title: "Create todo", message: "", preferredStyle: .alert)
+        alert.addTextField()
+        let saveButton = UIAlertAction(title: "Save", style: .default) { _ in
+            if let textName = alert.textFields?.first?.text {
+                self.addTodo(name: textName)
+        }
+    }
+        alert.addAction(saveButton)
+        let cancelButton = UIAlertAction(title: "Cancel", style: .destructive)
+        alert.addAction(cancelButton)
+            
+        present(alert, animated: true)
+    }
+    
+// MARK: - Table View Methods
+    
+// Returns the number of rows in the table (equal to the number of elements in `todos`).
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todos.count
     }
     
+// Returns the cell for the specified row in the table. Fills the cell with data from `todos`.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "todoCell")
         let todoItem = todos[indexPath.row]
@@ -80,8 +90,9 @@ class ToDoViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+// Handles the action of deleting a row in a table. Removes a task from `todos` and updates the table.
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete{
+        if editingStyle == .delete {
             todos.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
