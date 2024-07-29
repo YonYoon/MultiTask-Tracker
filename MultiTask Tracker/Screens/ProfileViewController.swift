32a93db997e4ea1e3218db4d4a9a505a3654c92d
YonYoon <zhzhzh1806@gmail.com>
2024-07-29 22:25:41 +0500

    Add sign out button to ProfileViewController

--- MultiTask Tracker/Screens/ProfileViewController.swift
+++ MultiTask Tracker/Screens/ProfileViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    let signOutButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        configureSignOutButton()
+    }
+    
+    private func configureSignOutButton() {
+        view.addSubview(signOutButton)
+        signOutButton.translatesAutoresizingMaskIntoConstraints = false
         
+        signOutButton.setTitle("Sign out", for: .normal)
         
+        signOutButton.addTarget(self, action: #selector(signOut), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            signOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signOutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            signOutButton.widthAnchor.constraint(equalToConstant: 100),
+            signOutButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    @objc private func signOut() {
+        AuthenticationManager.shared.signOut()
     }
 }
