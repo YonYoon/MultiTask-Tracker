af114567e1a74fa0d31fcea3aec3d34276313280
YonYoon <zhzhzh1806@gmail.com>
2024-08-17 12:36:47 +0500

    Stop using firebase emulator Cause: done testing auth

--- MultiTask Tracker/AppDelegate.swift
+++ MultiTask Tracker/AppDelegate.swift
@@ -18,7 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
+        
+        // For using firebase emulator
+//        Auth.auth().useEmulator(withHost: "127.0.0.1", port: 9099)
         return true
     }
 
