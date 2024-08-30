3ef192a812b33eef5c2419ed00da442b0105cd58
YonYoon <zhzhzh1806@gmail.com>
2024-08-30 17:52:07 +0500

    Fix search field and add search button

--- MultiTask Tracker/Screens/FriendSearchViewController.swift
+++ MultiTask Tracker/Screens/FriendSearchViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class FriendSearchViewController: UIViewController {
     var searchField = MTTextField()
+    var searchButton = MTButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = false
         
         configureSearchField()
+        configureSearchButton()
     }
     
     private func configureSearchField() {
@@ -24,13 +27,27 @@ class FriendSearchViewController: UIViewController {
         searchField.translatesAutoresizingMaskIntoConstraints = false
         searchField.autocorrectionType = .no
         searchField.autocapitalizationType = .none
-        searchField.placeholder = "Username"
+        searchField.placeholder = "Type your friend's username"
         
         NSLayoutConstraint.activate([
-            searchField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            searchField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             searchField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             searchField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
             searchField.heightAnchor.constraint(equalToConstant: 55)
+        ])
+    }
+    
+    private func configureSearchButton() {
+        view.addSubview(searchButton)
+        
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        searchButton.setTitle("Search", for: .normal)
+        
+        NSLayoutConstraint.activate([
+            searchButton.topAnchor.constraint(equalTo: searchField.bottomAnchor, constant: 15),
+            searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            searchButton.heightAnchor.constraint(equalToConstant: 55)
         ])
     }
 }
