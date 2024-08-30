0d6494d1236fc90a9f4e74599d654df821beca63
YonYoon <zhzhzh1806@gmail.com>
2024-08-30 17:55:47 +0500

    Create MTImageView

---
+++ MultiTask Tracker/MTImageView.swift
@@ -0,0 +1,20 @@
+//
+//  MTImageView.swift
+//  MultiTask Tracker
+//
+//  Created by Zhansen Zhalel on 30.08.2024.
+//
+
+import UIKit
+
+class MTImageView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
