81ab69a9a49edc8cc4bebeff19da93267c4a5bf2
YonYoon <zhzhzh1806@gmail.com>
2024-08-30 17:54:19 +0500

    Create FriendProfileViewController

---
+++ MultiTask Tracker/FriendProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FriendProfileViewController.swift
+//  MultiTask Tracker
+//
+//  Created by Zhansen Zhalel on 30.08.2024.
+//
+
+import UIKit
+
+class FriendProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
