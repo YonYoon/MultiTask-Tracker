aaed2f74be1493e52de39bb4aa7af90512224c18
YonYoon <zhzhzh1806@gmail.com>
2024-09-07 15:13:21 +0500

    Add display of current username

---
+++ MultiTask Tracker/Managers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  MultiTask Tracker
+//
+//  Created by Zhansen Zhalel on 07.09.2024.
+//
+
+import Foundation
--- MultiTask Tracker/Screens/ProfileViewController.swift
+++ MultiTask Tracker/Screens/ProfileViewController.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    let usernameLabel = UILabel()
     let signOutButton = UIButton(type: .system)
+    let user = AuthenticationManager.shared.user
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        configureUsernameLabel()
         configureSignOutButton()
+    }
+    
+    private func configureUsernameLabel() {
+        view.addSubview(usernameLabel)
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        usernameLabel.text = user?.displayName ?? "No name"
+        usernameLabel.textAlignment = .center
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            usernameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
     }
     
     private func configureSignOutButton() {
