f321bdc0ac9088d40f5eea3e85ace88c5910f920
YonYoon <zhzhzh1806@gmail.com>
2024-09-07 15:15:14 +0500

    Make NetworkManager to fetch user from Firestore

--- MultiTask Tracker/Managers/NetworkManager.swift
+++ MultiTask Tracker/Managers/NetworkManager.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private let db = Firestore.firestore()
+    
+    private init() { }
+    
+    func getUser(withName name: String) async -> MTUser! {
+        do {
+            let querySnapshot = try await db.collection("users").whereField("name", isEqualTo: name).getDocuments()
+            
+            let userDocument = querySnapshot.documents.first
+            if let documentId = userDocument?.documentID {
+                let user = try await db.collection("users").document(documentId).getDocument(as: MTUser.self)
+                return user
+            }
+            
+            return nil
+        } catch {
+            print("Query failed")
+            return nil
+        }
+    }
+}
