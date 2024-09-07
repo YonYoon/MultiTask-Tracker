dbf70f94bc50e1c769dd504fc93a00dad741978f
YonYoon <zhzhzh1806@gmail.com>
2024-09-07 11:30:59 +0500

    Add init for NSCoder

--- MultiTask Tracker/MTImageView.swift
+++ MultiTask Tracker/MTImageView.swift
@@ -15,6 +15,10 @@ class MTImageView: UIImageView {
         configure()
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private func configure() {
         layer.cornerRadius = 10
         clipsToBounds = true
