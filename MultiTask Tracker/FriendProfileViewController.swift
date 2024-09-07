0fdefb63d69da13ad0a5225754cca6109318cf9b
YonYoon <zhzhzh1806@gmail.com>
2024-09-07 11:32:06 +0500

    Configure avatar and username label in FriendProfileViewController

--- MultiTask Tracker/FriendProfileViewController.swift
+++ MultiTask Tracker/FriendProfileViewController.swift
@@ -8,22 +8,57 @@
 import UIKit
 
 class FriendProfileViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    let avatarImageView = MTImageView(frame: .zero)
+    let usernameLabel = UILabel()
+    
+    var user: MTUser!
+    
+    init(user: MTUser!) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureAvatarImageView()
+        configureUsernameLabel()
+    }
+    
+    private func configureAvatarImageView() {
+        view.addSubview(avatarImageView)
+        avatarImageView.tintColor = .lightGray
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90)
+        ])
+    }
+    
+    private func configureUsernameLabel() {
+        view.addSubview(usernameLabel)
+        usernameLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        usernameLabel.textAlignment = .natural
+        usernameLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+        usernameLabel.textColor = .label
+        usernameLabel.adjustsFontSizeToFitWidth = true
+        usernameLabel.minimumScaleFactor = 0.9
+        usernameLabel.lineBreakMode = .byTruncatingTail
+        
+        usernameLabel.text = user.name
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            usernameLabel.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 12),
+            usernameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
+    }
 }
