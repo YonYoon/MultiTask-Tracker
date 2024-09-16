4f37e830d30f51e8617a88f22368fd1e5218bd2d
YonYoon <zhzhzh1806@gmail.com>
2024-09-16 10:33:37 +0500

    Change ToDo in MainTabBar to ReminderList

--- MultiTask Tracker/MainTabBarController.swift
+++ MultiTask Tracker/MainTabBarController.swift
@@ -31,7 +31,7 @@ class MainTabBarController: UITabBarController {
     }
     
     private func setupTabs() {
-        let toDo = self.createNavigaiontController(title: "ToDo", image: UIImage(named: "toDoPic"), viewController: ToDoViewController())
+        let toDo = self.createNavigaiontController(title: "ToDo", image: UIImage(named: "toDoPic"), viewController: ReminderListViewController())
         let pomodoro = createNavigaiontController(title: "Pomodoro", image: UIImage(systemName: "timer"), viewController: PomodoroViewController())
         let friends = self.createNavigaiontController(title: "Friends", image: UIImage(named: "friendsPic"), viewController: FriendsViewController())
         let account = self.createNavigaiontController(title: "Profile", image: UIImage(named: "accountPic"), viewController: ProfileViewController())
